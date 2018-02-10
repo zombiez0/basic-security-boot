ae24e8bf1282c01ef02a3b3802296af9f3b8e83e
zombiez0 <chetananand.a@gmail.com>
2018-02-10 21:09:53 +0530

    Initial commit

---
+++ src/main/java/com/example/security/basic/config/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2018 General Electric Company. All rights reserved.
+ *
+ * The copyright to the computer software herein is the property of
+ * General Electric Company. The software may be used and/or copied only
+ * with the written permission of General Electric Company or in accordance
+ * with the terms and conditions stipulated in the agreement/contract
+ * under which the software has been supplied.
+ */
+ 
+package com.example.security.basic.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter
+{
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+            .withUser("admin").password("admin").roles("ADMIN").and()
+            .withUser("user").password("user").roles("USER");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception
+    {
+        httpSecurity        
+        .authorizeRequests()
+        .antMatchers("**/demo").hasRole("USER")
+        .anyRequest()
+        //.permitAll()  this permits all requests
+        .fullyAuthenticated()
+        .and().httpBasic();
+        httpSecurity.csrf().disable();
+    }
+}
---
+++ src/main/java/com/example/security/basic/endpoints/DataController.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 General Electric Company. All rights reserved.
+ *
+ * The copyright to the computer software herein is the property of
+ * General Electric Company. The software may be used and/or copied only
+ * with the written permission of General Electric Company or in accordance
+ * with the terms and conditions stipulated in the agreement/contract
+ * under which the software has been supplied.
+ */
+ 
+package com.example.security.basic.endpoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/hello")
+public class DataController
+{
+    @GetMapping("/")
+    public String hello()
+    {
+        return "Hello";
+    }
+}
